438acc4759ca8d990e38b59805ec7b747c60e7e5
Pieter Delobelle <pieter.delobelle@gmail.com>
2020-07-20 11:36:45 +0200

    [UI] CompareRunView: added button to remove all parameters in parallelPlot

--- mlflow/server/js/src/experiment-tracking/components/ParallelCoordinatesPlotControls.js
+++ mlflow/server/js/src/experiment-tracking/components/ParallelCoordinatesPlotControls.js
@@ -1,56 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TreeSelect } from 'antd';
+import {Icon, TreeSelect} from 'antd';
+import {IconButton} from "../../common/components/IconButton";
 
 export class ParallelCoordinatesPlotControls extends React.Component {
-  static propTypes = {
-    // An array of available parameter keys to select
-    paramKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
-    // An array of available metric keys to select
-    metricKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
-    selectedParamKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
-    selectedMetricKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
-    handleParamsSelectChange: PropTypes.func.isRequired,
-    handleMetricsSelectChange: PropTypes.func.isRequired,
-  };
+    static propTypes = {
+        // An array of available parameter keys to select
+        paramKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
+        // An array of available metric keys to select
+        metricKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
+        selectedParamKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
+        selectedMetricKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
+        handleParamsSelectChange: PropTypes.func.isRequired,
+        handleMetricsSelectChange: PropTypes.func.isRequired,
+    };
 
-  static handleFilterChange = (text, option) =>
-    option.props.title.toUpperCase().includes(text.toUpperCase());
+    static handleFilterChange = (text, option) =>
+        option.props.title.toUpperCase().includes(text.toUpperCase());
 
-  render() {
-    const {
-      paramKeys,
-      metricKeys,
-      selectedParamKeys,
-      selectedMetricKeys,
-      handleParamsSelectChange,
-      handleMetricsSelectChange,
-    } = this.props;
-    return (
-      <div className='plot-controls'>
-        <div>Parameters:</div>
-        <TreeSelect
-          className='metrics-select'
-          searchPlaceholder='Please select parameters'
-          value={selectedParamKeys}
-          showCheckedStrategy={TreeSelect.SHOW_PARENT}
-          treeCheckable
-          treeData={paramKeys.map((k) => ({ title: k, value: k, label: k }))}
-          onChange={handleParamsSelectChange}
-          filterTreeNode={ParallelCoordinatesPlotControls.handleFilterChange}
-        />
-        <div style={{ marginTop: 20 }}>Metrics:</div>
-        <TreeSelect
-          className='metrics-select'
-          searchPlaceholder='Please select metrics'
-          value={selectedMetricKeys}
-          showCheckedStrategy={TreeSelect.SHOW_PARENT}
-          treeCheckable
-          treeData={metricKeys.map((k) => ({ title: k, value: k, label: k }))}
-          onChange={handleMetricsSelectChange}
-          filterTreeNode={ParallelCoordinatesPlotControls.handleFilterChange}
-        />
-      </div>
-    );
-  }
+    render() {
+        const {
+            paramKeys,
+            metricKeys,
+            selectedParamKeys,
+            selectedMetricKeys,
+            handleParamsSelectChange,
+            handleMetricsSelectChange,
+        } = this.props;
+        return (
+            <div className='plot-controls'>
+                <div>Parameters:
+                    <IconButton
+                        icon={<i className='far fa-minus-square'/>}
+                        onClick={(e) => handleParamsSelectChange([])}
+                        style={{marginLeft: '1em'}}
+                    /></div>
+                <TreeSelect
+                    className='metrics-select'
+                    searchPlaceholder='Please select parameters'
+                    value={selectedParamKeys}
+                    showCheckedStrategy={TreeSelect.SHOW_PARENT}
+                    treeCheckable
+                    treeData={paramKeys.map((k) => ({title: k, value: k, label: k}))}
+                    onChange={handleParamsSelectChange}
+                    filterTreeNode={ParallelCoordinatesPlotControls.handleFilterChange}
+                />
+
+                <div style={{marginTop: 20}}>Metrics:</div>
+                <TreeSelect
+                    className='metrics-select'
+                    searchPlaceholder='Please select metrics'
+                    value={selectedMetricKeys}
+                    showCheckedStrategy={TreeSelect.SHOW_PARENT}
+                    treeCheckable
+                    treeData={metricKeys.map((k) => ({title: k, value: k, label: k}))}
+                    onChange={handleMetricsSelectChange}
+                    filterTreeNode={ParallelCoordinatesPlotControls.handleFilterChange}
+                />
+            </div>
+        );
+    }
 }
